1cf72ec71f1d0bea859fdbd71d9a6fcbc7ea4cab
Adrian <adrian.schnyder@students.fhnw.ch>
2022-10-16 00:58:57 +0200

    Update GUI.py
    does not update playerentry field

--- GUI.py
+++ GUI.py
@@ -1,6 +1,6 @@
 
 import numbers
-import tkinter
+import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 import wargameChoises as wgc
@@ -9,18 +9,22 @@ maxRowlen = 2
 
 wgc.loadMaps()
 wgc.loadCountries()
-players = 0
+
 
 
 root = Tk()
+root.title("Wargame RD Match roller")
 frm = ttk.Frame(root, padding=10)
 frm.grid()
-ttk.Label(frm, text="Amount of Players").grid(column=0, row=0)
-playercountField = ttk.Entry(frm,textvariable=numbers).grid(column=1, row=0)
+playercountField = tk.IntVar() #TODO does not get updated?
 
 def runScript():
-    players = int(playercountField.get())
+    players = playercountField.get()  
+    print(players)
     
+def wgcStuff(players):
+    if players < 2:
+        players = 2
     wgc.rollmap(players)
     wgc.initializePlayers(players)
     wgc.addAllowedCountires()
@@ -28,12 +32,18 @@ def runScript():
     
 def updateTTk():
     ttk.Label(frm, text="Map").grid(column=0, row=0)
-    ttk.Label(frm, text=(wgc.map["Name"],wgc.map["Size(Given)"],wgc.map["Type"])).grid(column=1, row=0)
+    map = wgc.rollmap(2)
+    ttk.Label(frm, text=(map["Name"],map["Size(Given)"],map["Type"])).grid(column=1, row=0)
     for i,player in enumerate(wgc.players):
         ttk.Label(frm, text=("Player",i+1,":")).grid(column=0, row=i+1)
         ttk.Label(frm, text=(player)).grid(column=1, row=i+1)
         maxRowlen = maxRowlen+1
 
-ttk.Button(frm, text="Roll Dice", command=runScript()).grid(column=0, row=maxRowlen)
+ttk.Label(frm, text="Amount of Players").grid(column=0, row=0)
+entryPlayercountField = ttk.Entry(frm,textvariable=playercountField,text = "Players ?",width=5).grid(column=1, row=0)
+ttk.Label(frm, text=playercountField.get()).grid(column=2, row=0)
+
+
+ttk.Button(frm, text="Roll Dice", command=runScript).grid(column=0, row=maxRowlen)
 ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=maxRowlen)
 root.mainloop()
