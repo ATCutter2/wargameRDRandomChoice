c039308aa546e71ad21bbe77fe50c7d625b35e27
Adrian <adrian.schnyder@students.fhnw.ch>
2022-10-15 22:15:14 +0200

    changed functions to def

--- wargameChoises.py
+++ wargameChoises.py
@@ -4,8 +4,8 @@ import random
 import csv
 import math
 #Changable Variables
-playercount = 2
-enableCoalitions = 1
+#playercount = 2
+#enableCoalitions = 1
 #END Changable Variables
 
 forces = ["Redfor","Bluefor"]
@@ -13,66 +13,75 @@ forces = ["Redfor","Bluefor"]
 maps1v1 = []
 enumPvPNumbers = ["(1v1)"]
 colummnamesMaps = []
-with open('WargameRDMaps.csv', mode='r') as infile:
-    reader = csv.reader(infile,delimiter=';')
-    line_count = 0
-    for i,rows in enumerate(reader): 
-        if i == 0:
-            colummnamesMaps.append(rows[:])
-        else:
-            if rows[1] == enumPvPNumbers[math.floor(playercount/2)-1]:
-                maps1v1.append({"Name":rows[0],
-                                "Size(usable as)":rows[1],
-                                "Size(Given)":rows[2],
-                                "Type":rows[3:]})
+def loadMaps() :
+    with open('WargameRDMaps.csv', mode='r') as infile:
+        reader = csv.reader(infile,delimiter=';')
+        line_count = 0
+        for i,rows in enumerate(reader): 
+            if i == 0:
+                colummnamesMaps.append(rows[:])
+            else:
+                if rows[1] == enumPvPNumbers[math.floor(playercount/2)-1]:
+                    maps1v1.append({"Name":rows[0],
+                                    "Size(usable as)":rows[1],
+                                    "Size(Given)":rows[2],
+                                    "Type":rows[3:]})
+
 
 #Load Countries
 colummnames = []
 specialisation = []
 force = []
-with open('Factionlist.csv', mode='r') as infile:
-    reader = csv.reader(infile,delimiter=';')
-    line_count = 0
-    for i,rows in enumerate(reader): 
-        if i == 0:
-            colummnames.append(rows[:])
-            for spec in range(3,len(rows)):
-                specialisation.append(rows[spec])    
-            continue
-        else:
-            force.append({"faction":rows[0],
-                          "isCoalition":rows[1],
-                          "Coalition":rows[2],
-                          "specialisation":rows[3:]})
+def loadCountries():
+    with open('Factionlist.csv', mode='r') as infile:
+        reader = csv.reader(infile,delimiter=';')
+        line_count = 0
+        for i,rows in enumerate(reader): 
+            if i == 0:
+                colummnames.append(rows[:])
+                for spec in range(3,len(rows)):
+                    specialisation.append(rows[spec])    
+                continue
+            else:
+                force.append({"faction":rows[0],
+                              "isCoalition":rows[1],
+                              "Coalition":rows[2],
+                              "specialisation":rows[3:]})
 
 
 #Initialize players
 players = []
-for i in range(0,playercount):
-    specialisationNr = specialisation.index(random.choice(specialisation))
-    faction = random.choice(forces)
-    players.append({"faction":faction,
-                    "specialisation":specialisation[specialisationNr],
-                    "specialisationNr":specialisationNr}
-                   )
+def initializePlayers(playercount=2):
+    for i in range(0,playercount):
+        specialisationNr = specialisation.index(random.choice(specialisation))
+        faction = random.choice(forces)
+        players.append({"faction":faction,
+                        "specialisation":specialisation[specialisationNr],
+                        "specialisationNr":specialisationNr}
+                       )
 
-#add allowed lands
-for i,p in enumerate(players):
-   #find allowed Countries
-    elligiblefactions = []
-    for forc in force:
-        if forc["faction"] == p["faction"]:
-            if forc["specialisation"][p["specialisationNr"]] == '1':
-                elligiblefactions.append((forc["Coalition"],forc["specialisation"]))
-    if len(elligiblefactions) >0 :
-        p.update({"Coalition":random.choice(elligiblefactions)})
-    else :
-        print("error no elligible factions")
-    #for e in elligiblefactions:
-    #    print(" ",e)
+#add allowed Counties
+def addAllowedCountires(enableCoalitions=1):
+    for i,p in enumerate(players):
+       #find allowed Countries
+        elligiblefactions = []
+        for forc in force:
+            if forc["faction"] == p["faction"]:
+                if forc["specialisation"][p["specialisationNr"]] == '1':
+                    elligiblefactions.append((forc["Coalition"],forc["specialisation"]))
+        if len(elligiblefactions) >0 :
+            p.update({"Coalition":random.choice(elligiblefactions)})
+        else :
+            print("error no elligible factions")
+        #for e in elligiblefactions:
+        #    print(" ",e)
     
 
 if __name__ == "__main__":
+    loadMaps()
+    loadCountries()
+    initializePlayers(2)
+    addAllowedCountires()
     map = random.choice(maps1v1)
     print("map:",map["Name"],map["Size(Given)"],map["Type"])
     #print(specialisation)
