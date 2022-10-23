485889996f357a3eb159b68a7c9995bde85f9541
Adrian <adrian.schnyder@students.fhnw.ch>
2022-10-24 01:57:22 +0200

    shouldWork now

--- GUI.py
+++ GUI.py
@@ -7,9 +7,12 @@ import wargameChoises as wgc
 
 maxRowlen = 2
 
-wgc.loadMaps()
-wgc.loadCountries()
-
+maps = wgc.loadMaps()
+factions,specialisations = wgc.loadCountries()
+constrictChoices = {"Coalitions"            : 1,
+                    "Banned Specialisations":[""],
+                    "Banned Countires"      :[],
+                }
 
 
 root = Tk()
@@ -17,33 +20,37 @@ root.title("Wargame RD Match roller")
 frm = ttk.Frame(root, padding=10)
 frm.grid()
 playercountField = tk.IntVar() #TODO does not get updated?
+def layoutInputs():
+    ttk.Label(frm, text="Amount of Players").grid(column=0, row=0)
+    entryPlayercountField = ttk.Entry(frm,textvariable=playercountField,text = "Players ?",width=5).grid(column=1, row=0)
+    ttk.Label(frm, text=playercountField.get()).grid(column=2, row=0)
+def layout(maxRowlength):
+    ttk.Button(frm, text="Roll Dice", command=runScript).grid(column=0, row=maxRowlength)
+    ttk.Button(frm, text="Quit",   command=root.destroy).grid(column=1, row=maxRowlength)    
+def updateTTk(playercount,maxRowlength):
+    constrictChoices.update({"Banned Specialisations": ["Support"]}) #Todo Make not static
+    rowMap = 1
+    ttk.Label(frm, text="Map").grid(column=0, row=rowMap)
+    map = wgc.rollmap(maps,playercount=playercount)
+    ttk.Label(frm, text=(map["Name"]+" | "+map["Type"])).grid(column=1, row=rowMap)
+    
+    rowPlayer = 3
+    players=wgc.rollplayers(factions,specialisations,constrictChoices,playercount=playercount)
+    print(players)
+    for i,p in enumerate(players):
+        ttk.Label(frm, text=("Player",i+1,":")).grid(                         column=0, row=i+rowPlayer)
+        ttk.Label(frm, text=(p["Name"]+" | "+p["Side"]+" | "+p["Type"])).grid(column=1, row=i+rowPlayer)
+        maxRowlength = maxRowlength+1
+    maxRowlength = maxRowlength+2
+    layout(maxRowlength=maxRowlength)
 
 def runScript():
-    players = playercountField.get()  
-    print(players)
-    
-def wgcStuff(players):
-    if players < 2:
-        players = 2
-    wgc.rollmap(players)
-    wgc.initializePlayers(players)
-    wgc.addAllowedCountires()
-    updateTTk()
-    
-def updateTTk():
-    ttk.Label(frm, text="Map").grid(column=0, row=0)
-    map = wgc.rollmap(2)
-    ttk.Label(frm, text=(map["Name"],map["Size(Given)"],map["Type"])).grid(column=1, row=0)
-    for i,player in enumerate(wgc.players):
-        ttk.Label(frm, text=("Player",i+1,":")).grid(column=0, row=i+1)
-        ttk.Label(frm, text=(player)).grid(column=1, row=i+1)
-        maxRowlen = maxRowlen+1
-
-ttk.Label(frm, text="Amount of Players").grid(column=0, row=0)
-entryPlayercountField = ttk.Entry(frm,textvariable=playercountField,text = "Players ?",width=5).grid(column=1, row=0)
-ttk.Label(frm, text=playercountField.get()).grid(column=2, row=0)
-
-
-ttk.Button(frm, text="Roll Dice", command=runScript).grid(column=0, row=maxRowlen)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=maxRowlen)
+    playercount = playercountField.get()  
+    print(playercount)
+    for widget in frm.winfo_children():
+        widget.destroy()
+    #layoutInputs()
+    updateTTk(playercount=2,maxRowlength=maxRowlen)
+ 
+layout(maxRowlen)
 root.mainloop()
--- wargameChoises.py
+++ wargameChoises.py
@@ -1,102 +1,100 @@
 
 import fractions
+from multiprocessing.sharedctypes import Value
+from secrets import choice
+from xmlrpc.client import Boolean
 import numpy as np
 import random
 import pandas as pd
 import math
 #Changable Variables
-#playercount = 2
-#enableCoalitions = 1
+#constrictChoices = {"Coalitions"            : 1,
+#                    "Banned Specialisations":[],
+#                    "Banned Countires"      :[],
+#                    }
 #END Changable Variables
 
-
 #Load Maps
-
-def loadMaps() :
-    maps  = pd.read_csv('WargameRDMaps.csv',delimiter=";",index_col=0)
-
-def rollmap(playercount=2):
-    maps = maps[maps["Players per Side"] >= (math.floor(playercount/2))]        
-    map = random.choice(maps)
+def loadMaps() -> pd.DataFrame :
+    maps =  pd.read_csv('WargameRDMaps.csv',delimiter=";",index_col=0)
+    #maps.columns = maps.columns.str.strip() #Remove Trailing whitespaces
+    return maps
+def rollmapRaw(maps: pd.DataFrame, playercount=2) ->pd.DataFrame:
+    validmaps = maps[maps["Players per Side"] >= (math.floor(playercount/2))]        
+    map = validmaps.sample()
     return map
-    
+def rollmap(maps: pd.DataFrame, playercount=2):
+    map = rollmapRaw(maps, playercount=playercount)
+    return {"Name":map.index.values[0].strip(),
+            "Type":map["Type"].values[0]
+            }
 #Load Countries
-colummnamesFactionlist = []
-specialisation = []
-force = []
-factions = []
-def loadCountries():
-    factions= pd.read_csv('Factionlist.csv',delimiter=";",index_col=0)
+def loadCountries(filepath = 'Factionlist.csv'):
+    factions= pd.read_csv(filepath,delimiter=";",index_col=0)
     specialisations = factions.columns[2:]
-    for sp in specialisations:
-        
-    
-  #      reader = csv.reader(infile,delimiter=';')
-  #      line_count = 0
-  #      for i,rows in enumerate(reader): 
-  #          if i == 0:
-  #              colummnamesFactionlist.extend(rows[:])
-  #              for spec in range(3,len(rows)):
-  #                  specialisation.append(rows[spec])    
-  #              continue
-  #          else:
-  #              force.append({"faction":rows[0],
-  #                            "isCoalition":rows[1],
-  #                            "Coalition":rows[2],
-  #                            "specialisation":rows[3:]})
-  #  for f in force:
-  #      if f["faction"] not in factions:
-  #          factions.append(f["faction"])
+    return (factions,specialisations)
 
-#Initialize players needs countries
-players = []
-def initializePlayers(playercount=2):
-    for i in range(0,playercount):
-        specialisationNr = specialisation.index(random.choice(specialisation))
-        faction = random.choice(factions)
-        players.append({"faction"         :faction,
-                        "specialisation"  :specialisation[specialisationNr],
-                        "specialisationNr":specialisationNr}
-                       )
-def rerollplayers():
-    for p in players:
-        specialisationNr = specialisation.index(random.choice(specialisation))
-        faction = random.choice(factions)
-        p = {"faction"         :faction,
-             "specialisation"  :specialisation[specialisationNr],
-             "specialisationNr":specialisationNr}
-                       
-#add allowed Counties needs Players
-def addAllowedCountires(enableCoalitions=1):
-    for i,p in enumerate(players):
-       #find allowed Countries
-        elligiblefactions = []
-        for forc in force:
-            if forc["faction"] == p["faction"]:
-                if forc["specialisation"][p["specialisationNr"]] == '1':
-                    elligiblefactions.append((forc["Coalition"],forc["specialisation"]))
-        if len(elligiblefactions) >0 :
-            p.update({"Coalition":random.choice(elligiblefactions)})
-        else :
-            print("error no elligible factions")
-        #for e in elligiblefactions:
-        #    print(" ",e)
+# functions for selection
+def rollFaction(factions,specialisations):
+    specialisation = random.choice(specialisations)
+    faction        = factions.sample()
+    return{"faction"         :faction,
+           "specialisation"  :specialisation
+          }
+def validFaction(faction: pd.DataFrame,constrictChoices):
+    eval = faction["faction"].iloc[:,0] not in constrictChoices["Banned Countires"] #Compare names in column Faction of csv
+    #print(faction["specialisation"],constrictChoices["Banned Countires"],eval)
+    return eval
+def validSpecialisation(faction,constrictChoices):
+    eval = not(any(x in faction["specialisation"]for x in constrictChoices["Banned Specialisations"]))
+    #print(faction["specialisation"],constrictChoices["Banned Specialisations"],eval)
+    return eval    
+def selectRoll(factions:pd.DataFrame,specialisations:pd.DataFrame,constrictChoices):
+    faction = factions[factions["is Coalition"] <= constrictChoices["Coalitions"]] #limit if coalitions not allowed
+    #Make chance to get a side 50-50
+    sides = faction.drop_duplicates("Side")
+    side = sides.sample()
+    side = side["Side"]
     
+    choiceNotValid = True
+    while choiceNotValid:
+        faction   = rollFaction(factions,specialisations)                #Give a first suggestion
+        
+        vFactions = validFaction(faction,constrictChoices)               #is it a allowed country?
+        vSpeciali = validSpecialisation(faction,constrictChoices)        #is it a allowed Specialisation?
+        vSide     = (faction["faction"].values[0,0]) == (side.values[0]) #is the selected side?
+        choiceNotValid = not(vFactions or vSpeciali or vSide)            #reroll if not within cirteria
+        #print(choiceNotValid,vFactions,vSpeciali,vSide)
+    return faction
+def rollplayersRaw(factions,specialisations,constrictChoices,playercount=2):
+    players = []
+    for i in range(0,playercount):
+        players.append(selectRoll(factions,specialisations,constrictChoices))
+    return players
+def rollplayers(factions,specialisations,constrictChoices,playercount=2):
+    players = rollplayersRaw(factions,specialisations,constrictChoices,playercount=playercount)
+    pl = []
+    for p in players:
+        #print(p["faction"],p["specialisation"])
+        pl.append({ "Name":p["faction"].index.values[0].strip(),
+                    "Side":p["faction"].values[0,0].strip(),
+                    "Type":p["specialisation"].strip()
+                    } )
+    #print(pl)
+    return pl                      
 
 if __name__ == "__main__":
-    loadMaps()
-    loadCountries()   
-    map = rollmap(2) 
-    initializePlayers(2)
-    addAllowedCountires()
-    print(map)
-    print("map:",map["Name"],map["Size(Given)"],map["Type"])
-    #print(specialisation)
-    specializationArray = [0,0,0,0,0,0,0]
+    constrictChoices = {"Coalitions"            : 1,
+                        "Banned Specialisations":[""],
+                        "Banned Countires"      :[],
+                    }
+    maps = loadMaps()
+    factions,specialisations = loadCountries() 
+    
+    map = rollmap(maps,playercount=2) 
+    constrictChoices.update({"Banned Specialisations": ["Support"]})
+    players=rollplayers(factions,specialisations,constrictChoices,playercount=2)
+    
+    print(map["Name"],map["Type"],sep=" | ")
     for i,p in enumerate(players):
-        ##visualize what  colummn is chosen
-        #choosenSpezialisation = specializationArray
-        #choosenSpezialisation[p["specialisationNr"]] = 1
-        #print("choosenSpezialisation",choosenSpezialisation) # this has problems with compilerside simplifications
-        print("Player",i+1,":", p)
-        print()
+        print("Player",i+1,":",p["Name"],p["Side"],p["Type"])
